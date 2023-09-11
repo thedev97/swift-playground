153bfba37d6364ddddbf875c023ea894cdfc3f9e
Soumya <soumyamishra637@gmail.com>
2023-09-11 16:39:16 +0530

    Loop

---
+++ loop.swift
@@ -0,0 +1,25 @@
+// For loop
+for i in 1...10{
+    print("Hello \(i)")
+}
+
+// For loop uisng Array
+var arr = ["Apple", "Orange", "Bannana", "Grape"]
+for i in arr {
+    print(i)
+}
+
+// While loop
+var a = 1;
+var target = 10
+while a<=target {
+ print(a)   
+ a += 1
+}
+
+// Repeat While loop (DoWhile)
+
+repeat {
+   print(a)   
+   a += 1
+} while a<=target
