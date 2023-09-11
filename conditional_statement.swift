5dcfb53f068824a323f4013a5253209a923575b7
Soumya <soumyamishra637@gmail.com>
2023-09-11 16:10:24 +0530

    Variables and Conditioanl statements

---
+++ conditional_statement.swift
@@ -0,0 +1,6 @@
+var num = 10;
+if(num > 0) { print("Number is gretter than 0");
+if(num != 0) { print("True") }
+else { print("False") } }
+else if(num < 10) { print("Number is less than 0") }
+else { print("Default") }
---
+++ variables.swift
@@ -0,0 +1,25 @@
+var a = 12 // mutable 
+print("valu of a: ",a)
+a = 15
+print("change the value of a: " , a)
+
+let b = 20 // imutable/const value
+print(b)
+
+var i:Int = 10
+print(i);
+
+var d: Double = 10.1234567890123456789
+print(d)
+
+var f: Float = 10.123456789
+print(f)
+
+var str:String = "Soumya"
+print(str)
+
+var char: Character = "S"
+print(char)
+
+var isOn: Bool = false
+print(isOn)
